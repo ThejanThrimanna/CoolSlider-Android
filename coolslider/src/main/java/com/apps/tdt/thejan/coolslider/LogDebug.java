5f749abd45f94e95197275cdfb29ddbe31211cbc
Thejan Thrimanna <thejan.dthrimanna@gmail.com>
2018-07-22 21:00:26 +0530

    initial commit with adding LogDebug

---
+++ coolslider/src/main/java/com/apps/tdt/thejan/coolslider/LogDebug.java
@@ -0,0 +1,12 @@
+package com.apps.tdt.thejan.coolslider;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message){
+         Log.d(TAG, message);
+    }
+}
